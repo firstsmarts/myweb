db1d39c054ea2c6ac904cc37b5d8f3d6a0f804e0
chengkun <chengkun@antiy.cn>
2017-12-12 11:53:36 +0800

    host

--- myweb/settings.py
+++ myweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fm88*@=ej_uxyc7&r5#oa2+)0dj8b_$ia$liz%88$3yprpc(@d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
